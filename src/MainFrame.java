e1f288f2681e1eef6696ab18a64a629494abd6e6
Carl Lundholm <carl_0221@hotmail.se>
2024-12-01 14:31:29 +0100

    Added a main frame to the game.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+
+import javax.swing.JFrame;
 
 public class MainFrame {
+    private int rowCount = 21;
+    private int columnCount = 19;
+    private int titleSize = 32;
+    private int boardWidth = columnCount * titleSize;
+    private int boardHeight = rowCount * titleSize;
+
+    public MainFrame() {
+        JFrame frame = new JFrame("Pac Man");
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(boardWidth, boardHeight);
+
+
+        frame.setVisible(true);
+
+    }
+
+
 }
